52638e902c0fb0efd69319fbc43fbeed4a3e8189
Maxim Elushev <maxim.elushev@yandex.ru>
2022-12-28 00:12:37 +0300

    Добавил первый пример

---
+++ Examples/Example1/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // объявление переменных
+        int a = 88, b = -123, c;
+        c = a + b;
+        System.out.println("a + b = " + c);
+    }
+}
