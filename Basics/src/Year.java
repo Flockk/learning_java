fe9a223e38c93d1cc4401bfa23304a05092936cd
Maxim Elushev <maxim.elushev@yandex.ru>
2022-12-27 23:10:32 +0300

    Максимально простая тестовая программа

---
+++ Basics/src/Year.java
@@ -0,0 +1,12 @@
+public class Year {
+
+    public static void main(String[] args) {
+        int year = 2022;
+
+        if (year % 4 == 0) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
