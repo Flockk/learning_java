3d7502adf7c11212fc7fddcbaea273ef8a78dd9d
Maxim Elushev <maxim.elushev@yandex.ru>
2022-12-30 18:33:49 +0400

    Добавил четырнадцатый пример

---
+++ Examples/Example14/src/geometricfigures/Figure.java
@@ -0,0 +1,18 @@
+package geometricfigures;
+
+public abstract class Figure {
+
+    private float x;
+    private float y;
+
+    public Figure(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public abstract float getPerimeter();
+
+    public abstract float getArea();
+
+    public abstract void drawOnScreen();
+}
