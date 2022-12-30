88418047ec55396dd1e1c511c4cf02a924d568b2
Maxim Elushev <maxim.elushev@yandex.ru>
2022-12-30 13:01:28 +0400

    Добавил первое задание в ООП

---
+++ OOP/Exercise1/src/Dog.java
@@ -0,0 +1,35 @@
+class Dog {
+    private String breed;
+    private String size;
+    private int age;
+
+    public Dog(String breed, String size, int age) {
+        this.breed = breed;
+        this.size = size;
+        this.age = age;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ OOP/Exercise1/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Dog dog = new Dog("Mastiff", "big", 7);
+        dog.setBreed("WhiteMastiff");
+        dog.setSize("middle");
+        dog.setAge(5);
+        System.out.println(dog.getBreed() + " " + dog.getSize() + " " + dog.getAge());
+    }
+}
